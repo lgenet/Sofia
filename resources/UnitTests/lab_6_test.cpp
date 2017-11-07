8e59f95918c0ad3110201f6c2313575c58103f7d
lgenet <lgenet@spsu.edu>
2017-11-07 15:17:28 -0500

    Fixing breaking issues in grade flow

---
+++ resources/UnitTests/lab_6_test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "lab_for_testing.cpp"
+
+using namespace std;
+
+int main() {
+    cout << "In tester" << endl;
+    lab();
+    cout << "Done with testing lab 6" << endl;
+
+    return 0;
+}
